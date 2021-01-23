6e6ff8ce195ce90248abc871ddabd228e3c26185
jaflo <imjaflo@gmail.com>
2021-01-23 11:53:58 -0600

    Only run extension when in iframe

--- extension/inject.js
+++ extension/inject.js
@@ -1,21 +1,31 @@
-const browser = chrome || browser;
-
-browser.runtime.onMessage.addListener(({ type, value }) => {
-	if (type === "refresh-page") {
-		window.location = window.location;
-	} else if (type === "highlight-homework-number") {
-		const regex = new RegExp(`H*W\\D*${value}\\b`, "gi");
-		[...document.getElementsByTagName("a")].forEach((el) => {
-			const candidate = regex.exec(el.href);
-
-			if (candidate) {
-				el.style.borderRadius = "3px";
-				el.style.background = "#297373";
-				el.style.boxShadow = "0 0 0 10px #297373";
-				el.style.color = "#E6E6E6";
-			}
-		});
+function isEmbedded() {
+	try {
+		return window.self !== window.top;
+	} catch (e) {
+		return true;
 	}
-});
+}
 
-browser.runtime.sendMessage({ type: "url", value: document.location.href });
+if (isEmbedded()) {
+	const browser = chrome || browser;
+
+	browser.runtime.onMessage.addListener(({ type, value }) => {
+		if (type === "refresh-page") {
+			window.location = window.location;
+		} else if (type === "highlight-homework-number") {
+			const regex = new RegExp(`H*W\\D*${value}\\b`, "gi");
+			[...document.getElementsByTagName("a")].forEach((el) => {
+				const candidate = regex.exec(el.href);
+
+				if (candidate) {
+					el.style.borderRadius = "3px";
+					el.style.background = "#297373";
+					el.style.boxShadow = "0 0 0 10px #297373";
+					el.style.color = "#E6E6E6";
+				}
+			});
+		}
+	});
+
+	browser.runtime.sendMessage({ type: "url", value: document.location.href });
+}
